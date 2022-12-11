ccd6b8b14112799309e1d0de5dd7e1691cc64ac7
Hwang-Junsu <hjs77728@gmail.com>
2022-12-12 02:10:36 +0900

    Fix: valid error

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,29 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Input({
-  type,
-  value,
-  onChange,
-  placeholder,
-  required = true,
+    type,
+    value,
+    onChange,
+    placeholder,
+    required = true,
 }) {
-  return (
-    <StyledInput
-      type={type}
-      value={value || ""}
-      onChange={onChange}
-      placeholder={placeholder}
-      required={required}
-    />
-  );
+    return (
+        <StyledInput
+            type={type}
+            value={value || ""}
+            onChange={onChange}
+            placeholder={placeholder}
+            required={required}
+        />
+    );
 }
 
 const StyledInput = styled.input`
-  padding: 15px 30px;
-  border: 1px solid gray;
-  border-radius: 20px;
-  outline: none;
-  &:focus {
-    border: 2px solid purple;
-  }
+    width: 100%;
+    padding: 15px 30px;
+    border: 1px solid gray;
+    border-radius: 20px;
+    outline: none;
+    &:focus {
+        border: 2px solid purple;
+    }
 `;
--- src/libs/valid.js
+++ src/libs/valid.js
@@ -1,18 +1,18 @@
 export const isValidEmailForm = (email) => {
-  const emailRegax = new RegExp(
-    /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
-  );
-  return email !== "" && email !== "undefined" && emailRegax.test(email);
+    const emailRegax = new RegExp(
+        /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    );
+    return email !== "" && email !== "undefined" && emailRegax.test(email);
 };
 
 export const isValidPassword = (password) => {
-  const passwordRegax = new RegExp(
-    /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$/
-  );
-  return passwordRegax.test(password);
+    const passwordRegax = new RegExp(
+        /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$/
+    );
+    return passwordRegax.test(password);
 };
 
 export const isMatchedPassword = (pw1, pw2) => {
-  if (pw1 === "" && pw2 === "") return false;
-  return pw1 === pw2;
+    if (pw1 === "" && pw2 === "") return false;
+    return pw1 === pw2;
 };
